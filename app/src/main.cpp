3d000aa71e3ef0e3227a9d9b067b3607aa619eaa
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2026-08-18 22:08:05 +0200

    ex01-step03
    added a cooperative thread.
    Because it has the highest priority, and it never sleeps (it only yields), the other threads never run

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,9 +6,17 @@ LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 #define STACK_SIZE 1024
 
 static constexpr uint32_t defaultThreadStackSize{1024U};
-static constexpr uint32_t threadLowPriority{7U};
-static constexpr uint32_t threadMediumPriority{5U};
-static constexpr uint32_t threadHighPriority{3U};
+static constexpr int32_t threadLowPriority{7};
+static constexpr int32_t threadMediumPriority{5};
+static constexpr int32_t threadHighPriority{3};
+static constexpr int32_t threadCooperativePriority{-1};
+
+
+void doWork()
+{
+    k_busy_wait(2000);
+    return;
+}
 
 void threadLowPriorityBody(void *p1, void *p2, void *p3)
 {
@@ -34,13 +42,27 @@ void threadHighPriorityBody(void *p1, void *p2, void *p3)
     }
 }
 
+void cooperativeThreadBody(void *p1, void *p2, void *p3)
+{
+    while (1) {
+        LOG_INF("Cooperative thread running");
+        for (uint32_t index = 0; index < 5; ++index) {
+            doWork();
+        }
+        LOG_INF("Cooperative thread yielding");
+        k_yield();
+    }
+}
+
+
 K_THREAD_DEFINE(thread_a, defaultThreadStackSize, threadLowPriorityBody,
                 NULL, NULL, NULL, threadLowPriority, 0, 0);
 K_THREAD_DEFINE(thread_b, defaultThreadStackSize, threadMediumPriorityBody,
                 NULL, NULL, NULL, threadMediumPriority, 0, 0);
 K_THREAD_DEFINE(thread_c, defaultThreadStackSize, threadHighPriorityBody,
                 NULL, NULL, NULL, threadHighPriority, 0, 0);
-
+K_THREAD_DEFINE(thread_d, defaultThreadStackSize, cooperativeThreadBody,
+                NULL, NULL, NULL, threadCooperativePriority, 0, 0);
 int main(void)
 {
     return 0;
