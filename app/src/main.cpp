e93253738220e87abaffae7e3cec8d14a26df89d
Pascal Roobrouck <pascal.roobrouck@gmail.com>
2026-08-31 13:56:59 +0200

    preparation for homework l2-1

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,68 +3,40 @@
 
 LOG_MODULE_REGISTER(demo, LOG_LEVEL_DBG);
 
-#define STACK_SIZE 1024
-
 static constexpr uint32_t defaultThreadStackSize{1024U};
-static constexpr int32_t threadLowPriority{7};
-static constexpr int32_t threadMediumPriority{5};
-static constexpr int32_t threadHighPriority{3};
-static constexpr int32_t threadCooperativePriority{-1};
+static constexpr int32_t threadPriority{5};
+static constexpr int32_t nmbrOfLoops{1000000U};
 
+static volatile uint64_t testCounter{};
+static struct k_sem ready;
 
-void doWork()
+void workerFunction(void *p1, void *p2, void *p3)
 {
-    k_busy_wait(2000);
-    return;
-}
-
-void threadLowPriorityBody(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("Low priority thread is running");
-        k_msleep(300);
+const char *threadName = k_thread_name_get(k_current_get());
+    LOG_INF("Thread %s started", threadName);
+    for (uint32_t index = 0; index < nmbrOfLoops; ++index) {
+        uint64_t temp = testCounter;
+        temp++;
+        testCounter = temp;
+        // k_yield();
     }
-}
 
-void threadMediumPriorityBody(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("Medium priority thread is running");
-        k_msleep(200);
-    }
-}
-
-void threadHighPriorityBody(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("High priority thread is running");
-        k_msleep(100);
-    }
-}
-
-void cooperativeThreadBody(void *p1, void *p2, void *p3)
-{
-    while (1) {
-        LOG_INF("Cooperative thread running");
-        for (uint32_t index = 0; index < 5; ++index) {
-            doWork();
-        }
-        LOG_INF("Cooperative thread yielding");
-        k_yield();
-    }
+    LOG_INF("Thread %s finished", threadName);
+    k_sem_give(&ready);
 }
 
 
-K_THREAD_DEFINE(thread_a, defaultThreadStackSize, threadLowPriorityBody,
-                NULL, NULL, NULL, threadLowPriority, 0, 0);
-K_THREAD_DEFINE(thread_b, defaultThreadStackSize, threadMediumPriorityBody,
-                NULL, NULL, NULL, threadMediumPriority, 0, 0);
-K_THREAD_DEFINE(thread_c, defaultThreadStackSize, threadHighPriorityBody,
-                NULL, NULL, NULL, threadHighPriority, 0, 0);
-K_THREAD_DEFINE(thread_d, defaultThreadStackSize, cooperativeThreadBody,
-                NULL, NULL, NULL, threadCooperativePriority, 0, 1000);
+K_THREAD_DEFINE(threadOne, defaultThreadStackSize, workerFunction, NULL, NULL, NULL, threadPriority, 0, 0);
+K_THREAD_DEFINE(threadTwo, defaultThreadStackSize, workerFunction, NULL, NULL, NULL, threadPriority, 0, 0);
+
+
 int main(void)
 {
+    k_sem_init(&ready, 0, 2);
+    LOG_INF("Starting threads : counter = %llu", testCounter);
+    k_sem_take(&ready, K_FOREVER);
+    k_sem_take(&ready, K_FOREVER);
+    LOG_INF("Threads ready : counter = %llu", testCounter);
     return 0;
 }
 
